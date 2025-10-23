9a7c64c43dab03513fc72ac502e1d5919330f5e3
Vargaviella <91993323+vargaviella@users.noreply.github.com>
2025-10-23 06:12:48 -0300

    Delete src/test directory

--- src/test/mocks/MockAlchemistAllocator.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
-
-import {AlchemistAllocator} from "../../AlchemistAllocator.sol";
-
-contract MockAlchemistAllocator is AlchemistAllocator {
-    constructor(address _myt, address _admin, address _operator) AlchemistAllocator(_myt, _admin, _operator) {}
-}
